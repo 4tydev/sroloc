b66c9b64b5bfaeeb96ca661646c9c5adbc35fe71
4tydev <70868650+4tydev@users.noreply.github.com>
2021-10-03 19:32:35 -0500

    Edited the console log of the running server

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(8080, () => {console.log("Game Server Started")});
+app.listen(8080, () => {console.log("Game Server Started at http://localhost:8080")});
 
