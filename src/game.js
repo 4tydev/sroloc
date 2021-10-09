72c5b4f327f09d5c6869f1558c826c1031942c9d
Mariano Hurtado de Mendoza <4tydev@gmail.com>
2021-10-08 19:20:14 -0500

    Added tiles to the game

--- src/game.js
+++ src/game.js
@@ -1,18 +1,31 @@
+const { FOREVER } = require("phaser");
 const Phaser = require("phaser");
 const ShipTransport = require("./classes/ShipTransport");
 
 const gameScene = new Phaser.Scene();
 
+const colors = ["Blue", "Green", "Red", "Yellow"];
+
 gameScene.init = function () {
   this.movementDirection = 1;
+  this.div4Tiles = [];
 };
+
 gameScene.preload = function () {
   this.load.spritesheet("background", "assets/AnimatedBackground.png", {
     frameWidth: 800,
     frameHeight: 600,
     endFrame: 72,
   });
+
   this.load.image("ship", "assets/sprites/SpaceShipDemo.png");
+
+  for (let i = 0; i < 4; i++) {
+    this.load.image(
+      colors[i].toLowerCase() + "TileDiv4",
+      "assets/tiles/Div4/" + colors[i] + "TileDiv4.png"
+    );
+  }
 };
 
 gameScene.create = function () {
@@ -25,14 +38,7 @@ gameScene.create = function () {
     frameRate: 20,
     repeat: Infinity,
   };
-
   this.anims.create(animConfig);
-
-  this.ships = this.add.group({
-    classType: ShipTransport,
-    maxSize: 1,
-    runChildUpdate: true,
-  });
 
   this.spacebar = this.input.keyboard.addKey(
     Phaser.Input.Keyboard.KeyCodes.SPACE
@@ -40,6 +46,23 @@ gameScene.create = function () {
   this.bg = this.add.sprite(400, 300, "background");
 
   this.bg.play("backgroundAnimation");
+
+  let between = 0;
+
+  for (let i = 0; i < 4; i++) {
+    this.div4Tiles.push(
+      this.add
+        .image(between, 100, colors[i].toLowerCase() + "TileDiv4")
+        .setOrigin(0, 0)
+    );
+    between += 200;
+  }
+
+  this.ships = this.add.group({
+    classType: ShipTransport,
+    maxSize: 1,
+    runChildUpdate: true,
+  });
 
   this.ship = this.add.image(400, 550, "ship");
   this.ship.setScale(0.25);
