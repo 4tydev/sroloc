aab8ac97d6c09f0e9f15b7fb249415a6e3ac9bb5
Mariano Hurtado de Mendoza <4tydev@gmail.com>
2021-11-11 22:53:12 -0600

    added procfile and changed server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const PORT = process.env.PORT | 5000;
 app.use("/dist", express.static(path.join(__dirname, "/dist")));
 app.use("/assets", express.static(path.join(__dirname, "/assets")));
 
