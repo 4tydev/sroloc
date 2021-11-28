15701f1fb6a43d538a4cfe50f1345328d44595a0
Mariano Hurtado de Mendoza <4tydev@gmail.com>
2021-11-27 18:37:24 -0600

    missing )

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT | 5000;
 const DIST_DIR = path.join(__dirname, '/dist');
 const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
-app.use("/dist", express.static(DIST_DIR);
+app.use("/dist", express.static(DIST_DIR));
 app.use("/assets", express.static(path.join(__dirname, "/assets")));
 
 app.get("*", (req, res) => {
