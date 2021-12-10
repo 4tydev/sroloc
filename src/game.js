3db5c9a3197e81290257e1fd7fbfcfbcff5ea4b9
Mariano Hurtado de Mendoza <4tydev@gmail.com>
2021-12-09 19:26:39 -0600

    Refactored code format

--- src/game.js
+++ src/game.js
@@ -1,6 +1,6 @@
 const spawningTilePositions = require("./spawning-tile-positions.json");
 
-const Phaser = require("phaser");
+const {Scene, Input, AUTO, Game} = require("phaser");
 const ShipTransport = require("./classes/ShipTransport");
 
 function getRandom(min, max) {
@@ -10,173 +10,14 @@ function getRandom(min, max) {
 }
 
 localStorage.setItem("score", 0);
-const gameScene = new Phaser.Scene();
+const MainScene = require("./classes/MainScene");
 
-gameScene.init = function () {
-  let styleSheet = document.getElementsByTagName("style")[0].sheet;
-  styleSheet.insertRule(
-    '@font-face{ font-family: "pixel"; src: url("/assets/PressStart2P.ttf"); format("truetype");'
-  );
-  this.movementStartDirection = getRandom(0, 2);
-  this.movementDirection;
-
-  let score = parseInt(localStorage.getItem("score"));
-
-  if (this.movementStartDirection === 0) {
-    this.movementDirection = 3 + (0.000001 + Math.sqrt(score));
-  } else {
-    this.movementDirection = -3 + (-0.000001 - Math.sqrt(score));
-  }
-
-  console.log(
-    `Score: ${localStorage.getItem("score")}: ${this.movementDirection}`
-  );
-
-  this.div4Tiles = [];
-  this.randNumTiles = getRandom(0, 24);
-  this.randNumShip = getRandom(0, 4);
-  this.tileColors = spawningTilePositions.Div4[this.randNumTiles];
-  this.mainColors = ["Blue", "Green", "Red", "Yellow"];
-  this.newShip;
-  this.tileRects = [];
-};
-
-gameScene.preload = function () {
-  this.load.script(
-    "webfont",
-    "https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
-  );
-  this.load.spritesheet("background", "assets/AnimatedBackground.png", {
-    frameWidth: 800,
-    frameHeight: 600,
-    endFrame: 72,
-  });
-
-  this.load.spritesheet("explosion", "assets/Explosion.png", {
-    frameWidth: 48,
-    frameHeight: 48,
-    endFrame: 7,
-  });
-
-  for (let i = 0; i < 4; i++) {
-    this.load.image(
-      this.tileColors[i].toLowerCase() + "TileDiv4",
-      "assets/tiles/Div4/" + this.tileColors[i] + "TileDiv4.png"
-    );
-  }
-
-  for (let i = 0; i < 4; i++) {
-    this.load.image(
-      this.mainColors[i].toLowerCase() + "SpaceShip",
-      "assets/sprites/" + this.mainColors[i] + "SpaceShip.png"
-    );
-  }
-};
-
-gameScene.create = function () {
-  this.scoreText;
-  let sceneRef = this;
-
-  WebFont.load({
-    custom: {
-      families: ["pixel"],
-    },
-    active: function () {
-      sceneRef.scoreText = sceneRef.add.text(
-        10,
-        10,
-        "Score: " + localStorage.getItem("score"),
-        { fontFamily: "pixel" }
-      );
-    },
-  });
-
-  var bgAnimConfig = {
-    key: "backgroundAnimation",
-    frames: this.anims.generateFrameNumbers("background", {
-      start: 0,
-      end: 72,
-    }),
-    frameRate: 20,
-    repeat: Infinity,
-  };
-
-  var explosionAnimConfig = {
-    key: "explosionAnimation",
-    frames: this.anims.generateFrameNumbers("explosion", {
-      start: 0,
-      end: 7,
-    }),
-    frameRate: 20,
-  };
-  this.anims.create(bgAnimConfig);
-  this.anims.create(explosionAnimConfig);
-
-  this.spacebar = this.input.keyboard.addKey(
-    Phaser.Input.Keyboard.KeyCodes.SPACE
-  );
-
-  this.touch
-  this.bg = this.add.sprite(400, 300, "background");
-
-  this.bg.play("backgroundAnimation");
-
-  let between = 0;
-
-  for (let i = 0; i < 4; i++) {
-    this.div4Tiles.push({
-      color: this.tileColors[i],
-      sprite: this.add
-        .sprite(between, 100, this.tileColors[i].toLowerCase() + "TileDiv4")
-        .setOrigin(0, 0),
-    });
-    this.div4Tiles[i].sprite.setDepth(1);
-    between += 200;
-  }
-
-  this.ships = this.add.group({
-    classType: ShipTransport,
-    maxSize: 1,
-    runChildUpdate: true,
-  });
-
-  this.ship = this.add.sprite(
-    400,
-    500,
-    this.mainColors[this.randNumShip].toLowerCase() + "SpaceShip"
-  );
-};
-
-gameScene.update = function () {
-  if (this.ship.x >= 750) {
-    this.movementDirection *= -1;
-  } else if (this.ship.x <= 50) {
-    this.movementDirection *= -1;
-  }
-
-  this.ship.x += this.movementDirection;
-
-  if (!this.newShip) {
-    if (Phaser.Input.Keyboard.JustDown(this.spacebar)) {
-      this.newShip = this.ships.get();
-
-      if (this.newShip) {
-        this.newShip.move(this.ship.x, this.ship.y);
-      }
-
-      this.ship.destroy();
-    }
-
-    this.newShip = undefined;
-  }
-};
-spacePressed = true;
 
 const config = {
-  type: Phaser.AUTO,
+  type: AUTO,
   width: 800,
   height: 600,
-  scene: gameScene,
+  scene: [ MainScene ],
 };
 
-const game = new Phaser.Game(config);
+const game = new Game(config);
